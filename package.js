a035dee3752a25fea837c4ec5eeb9bb4b83aef1e
parhelium <me@parhelium.pl>
2015-03-20 23:11:50 +0100

    bugfix : Ractive's component teardown function was never called

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'parhelium:ractive-in-blaze',
-  version: '0.0.3_1',
+  version: '0.0.4',
   summary: 'Allows for easy injection of Ractive component to Blaze templates.',
   git: 'https://github.com/parhelium/package-ractive-in-blaze',
   documentation: 'README.md'
