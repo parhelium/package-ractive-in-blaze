1020e673b618d3a8f5cca4d3334e01a38bc7f401
Kuba Wyrobek <kuba.wyrobek@gmail.com>
2015-05-07 13:05:13 +0200

    passed new parameter `options`, which allows to configure Ractive component.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'parhelium:ractive-in-blaze',
-  version: '0.0.4',
+  version: '0.0.5',
   summary: 'Allows for easy injection of Ractive component to Blaze templates.',
   git: 'https://github.com/parhelium/package-ractive-in-blaze',
   documentation: 'README.md'
